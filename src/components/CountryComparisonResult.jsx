db399cbe35a10fed4a42852560b929da29001257
Antonic3 <antonicarla4@gmail.com>
2024-10-11 14:56:34 +0700

    fix: result compare country

--- src/components/CountryComparisonResult.jsx
+++ src/components/CountryComparisonResult.jsx
@@ -16,92 +16,87 @@ export default function CompareResult({ data }) {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell sx={head}>
-              <div className="flex gap-5">
-                <img
-                  width={40}
-                  src={country1.flags.png}
-                  alt={`the flags of ${country1.name.common}`}
-                />
-                <h2>{country1.name.common}</h2>
-              </div>
-            </TableCell>
-            <TableCell sx={head}>
-              <div className="flex gap-5">
-                <img
-                  width={40}
-                  src={country2.flags.png}
-                  alt={`the flags of ${country2.name.common}`}
-                />
-                <h2>{country2.name.common}</h2>
-              </div>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell sx={head}>Code</TableCell>
-            <TableCell>{country1.cca2}</TableCell>
-            <TableCell>{country2.cca2}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={head}>Population</TableCell>
-            <TableCell>
-              {country1.population > country2.population ? (
-                <div className="flex gap-3">
-                  <i className="fa-solid fa-circle-check text-[#126e52]"></i>
-                  <p>{`${formatPopulation(country1.population)}`}</p>
+    <div className="flex gap-10">
+      <TableContainer component={Paper} sx={{ width: "50%" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={head}>Country 1 Information</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell sx={head}>Country Name</TableCell>
+              <TableCell>
+                <div className="flex gap-5">
+                  <img
+                    width={40}
+                    src={country1.flags.png}
+                    alt={`the flags of ${country1.name.common}`}
+                  />
+                  <h2>{country1.name.common}</h2>
                 </div>
-              ) : (
-                <p>{`${formatPopulation(country1.population)}`}</p>
-              )}
-            </TableCell>
-            <TableCell>
-              {country2.population > country1.population ? (
-                <div className="flex gap-3">
-                  <i className="fa-solid fa-circle-check text-[#126e52]"></i>
-                  <p>{`${formatPopulation(country2.population)}`}</p>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Country Code</TableCell>
+              <TableCell>{country1.cca2}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Population</TableCell>
+              <TableCell>{formatPopulation(country1.population)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Area</TableCell>
+              <TableCell>{formatArea(country1.area)} km²</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Region</TableCell>
+              <TableCell>{country1.region}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableContainer component={Paper} sx={{ width: "50%" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={head}>Country 2 Information</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell sx={head}>Country Name</TableCell>
+              <TableCell>
+                <div className="flex gap-5">
+                  <img
+                    width={40}
+                    src={country2.flags.png}
+                    alt={`the flags of ${country2.name.common}`}
+                  />
+                  <h2>{country2.name.common}</h2>
                 </div>
-              ) : (
-                <p>{`${formatPopulation(country2.population)}`}</p>
-              )}
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={head}>area</TableCell>
-            <TableCell>
-              {country1.area > country2.area ? (
-                <div className="flex gap-3">
-                  <i className="fa-solid fa-circle-check text-[#126e52]"></i>
-                  <p>{`${formatArea(country1.area)} km²`}</p>
-                </div>
-              ) : (
-                <p>{`${formatArea(country1.area)} km²`}</p>
-              )}
-            </TableCell>
-            <TableCell>
-              {country2.area > country1.area ? (
-                <div className="flex gap-3">
-                  <i className="fa-solid fa-circle-check text-[#126e52]"></i>
-                  <p>{`${formatArea(country2.area)} km²`}</p>
-                </div>
-              ) : (
-                <p>{`${formatArea(country2.area)} km²`}</p>
-              )}
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={head}>Region</TableCell>
-            <TableCell>{country1.region}</TableCell>
-            <TableCell>{country2.region}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Country Code</TableCell>
+              <TableCell>{country2.cca2}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Population</TableCell>
+              <TableCell>{formatPopulation(country2.population)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Area</TableCell>
+              <TableCell>{formatArea(country2.area)} km²</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={head}>Region</TableCell>
+              <TableCell>{country2.region}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
